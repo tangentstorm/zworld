5284f492465bba9211e08941ad130976dfeca94b
sabren <michal.wallace@gmail.com>
2013-03-18 20:47:52 -0500

    added a bunch of jittering cards

--- zwmain.pas
+++ zwmain.pas
@@ -27,14 +27,47 @@ var
 
 implementation
 
+type
+  TCard = class
+    bounds : TRect;
+    text   : String;
+    fg, bg : TBGRAPixel;
+    constructor create;
+  end;
+  TDeck = array of TCard;
+var
+  deck : TDeck;
+  i : integer;
+
+constructor TCard.Create;
+begin
+  bounds.Left := random( 800 );
+  bounds.Top  := random( 600 );
+  bounds.Right := bounds.left + 45;
+  bounds.Bottom := bounds.top + 25;
+  fg := rgb(clBlack);
+  bg := rgb(clWhite);
+  text := IntToHex(i, 4);
+end;
+
+
 {$R *.lfm}
 
 { TMainForm }
 
 procedure TMainForm.HeartBeatTimer( Sender : TObject );
+ var j : integer;
 begin
+  for i := low(deck) to high(deck) do
+    with deck[i].bounds do begin
+      j := random(3) - 1;
+      left += j; right += j;
+      j := random(3) - 1;
+      top += j; bottom += j;
+    end;
   Repaint
 end;
+
 
 procedure TMainForm.FormPaint( Sender : TObject );
 var
@@ -49,8 +82,20 @@ begin
   img.FontStyle := [fsBold];
   img.TextOut( 5, 5, 'ZoomWorld', rgb( $ff9933 ));
 
+
+  for i := low(deck) to high(deck) do
+    with deck[i] do
+    begin
+      img.FillRect(bounds, bg, dmSet);
+      img.TextRect(bounds, text, taCenter, tlCenter, fg )
+    end;
+
   img.Draw( self.canvas, 0, 0, True );
   img.Free;
 end;
 
+begin
+  SetLength( deck, 100 );
+  for i := low(deck) to high(deck) do
+     deck[i] := TCard.create;
 end.
